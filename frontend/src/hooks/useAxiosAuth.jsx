6f2801da3335c9b947f21f05b437404640559f9b
BanhMilos <448866426n@gmail.com>
2025-06-01 20:12:28 +0700

    fix useEffect call

--- frontend/src/hooks/useAxiosAuth.jsx
+++ frontend/src/hooks/useAxiosAuth.jsx
@@ -1,73 +1,82 @@
-import axios from 'axios';
-import { useAuth } from '../context/AuthContext';
+import axios from "axios";
+import { useMemo } from "react";
+import { useAuth } from "../context/AuthContext";
 
 const useAxiosAuth = () => {
   const { refresh } = useAuth();
 
-  const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    withCredentials: true, // nếu dùng cookie
-  });
-
-  axiosInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem('accessToken');
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  });
-  let isRefreshing = false;
-  let failedQueue = [];
-
-  const processQueue = (error, token) => {
-    failedQueue.forEach((prom) => {
-      if (error) {
-        prom.reject(error);
-      } else {
-        prom.resolve(token);
-      }
+  const axiosInstance = useMemo(() => {
+    const instance = axios.create({
+      baseURL: process.env.REACT_APP_API_URL,
+      withCredentials: true, // cookies
     });
 
-    failedQueue = [];
-  };
-
-  axiosInstance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-      const originalRequest = error.config;
-
-      if (error.response?.status === 401 && !originalRequest._retry) {
-        if (isRefreshing) {
-          return new Promise(function (resolve, reject) {
-            failedQueue.push({ resolve, reject });
-          })
-            .then((token) => {
-              originalRequest.headers.Authorization = `Bearer ${token}`;
-              return axiosInstance(originalRequest);
-            })
-            .catch((err) => {
-              return Promise.reject(err);
-            });
-        }
-
-        originalRequest._retry = true;
-        isRefreshing = true;
-
-        try {
-          const newAccessToken = await refresh(); // assumed to return token string
-          localStorage.setItem("accessToken", newAccessToken);
-          processQueue(null, newAccessToken);
-          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-          return axiosInstance(originalRequest);
-        } catch (err) {
-          processQueue(err, null);
-          return Promise.reject(err);
-        } finally {
-          isRefreshing = false;
-        }
+    instance.interceptors.request.use((config) => {
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
       }
+      return config;
+    });
 
-      return Promise.reject(error);
-    }
-  );
+    let isRefreshing = false;
+    let failedQueue = [];
+
+    const processQueue = (error, token) => {
+      failedQueue.forEach((prom) => {
+        if (error) {
+          prom.reject(error);
+        } else {
+          prom.resolve(token);
+        }
+      });
+      failedQueue = [];
+    };
+
+    instance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (!error.response) {
+          return Promise.reject(error);
+        }
+
+        if (error.response.status === 401 && !originalRequest._retry) {
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({ resolve, reject });
+            })
+              .then((token) => {
+                originalRequest.headers.Authorization = `Bearer ${token}`;
+                return instance(originalRequest); 
+              })
+              .catch((err) => Promise.reject(err));
+          }
+
+          originalRequest._retry = true;
+          isRefreshing = true;
+
+          try {
+            const newAccessToken = await refresh(); 
+            localStorage.setItem("accessToken", newAccessToken);
+            processQueue(null, newAccessToken);
+            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+            return instance(originalRequest); 
+          } catch (err) {
+            processQueue(err, null);
+            return Promise.reject(err);
+          } finally {
+            isRefreshing = false;
+          }
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    return instance;
+  }, [refresh]);
 
   return axiosInstance;
 };
